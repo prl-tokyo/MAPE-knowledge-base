b7a14ab281f361a41cc23704046ffac77f473bc8
Lionel Montrieux <lionel@nii.ac.jp>
2015-11-17 17:22:24 +0900

    added 'resource' param to PUT on monitor

--- mapekb/api.py
+++ mapekb/api.py
@@ -1,5 +1,9 @@
+"""An API for MAPE components to communicate with the knowledge base"""
 import hug
 
 @hug.put('/monitor', versions=1)
-def monitor(status):
+def monitor(resource, status):
+    """Indicate the status of a resource. The resource is identified by
+    its resource ID.
+    """
     return "OK"
