ee945f5aa979affb173bf6ae7aee3bb2ef865e7d
Lionel Montrieux <lionel@nii.ac.jp>
2015-11-20 18:35:26 +0900

    Default input format set to JSON

--- mapekb/api.py
+++ mapekb/api.py
@@ -1,6 +1,10 @@
 """An API for MAPE components to communicate with the knowledge base"""
 import hug
 
+@hug.default_input_format("application/json")
+def my_input_formatter(data):
+    return(hug.input_format.json(data))
+
 @hug.put('/monitor', versions=1)
 def monitor(resource, status):
     """Indicate the status of a resource. The resource is identified by its resource ID."""
