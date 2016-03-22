f53b82352371aa97110ddc63494af337ce8fb271
Lionel Montrieux <lionel@nii.ac.jp>
2016-03-22 16:51:54 +0900

    Update source model

--- models/cloud-bx/source/model.hs
+++ models/cloud-bx/source/model.hs
@@ -1,19 +1,41 @@
-data Model = Model [Reservation] [SecurityGroup]
-data Reservation = Reservation ID [SecurityGroupRef] [VM]
-data SecurityGroupRef = SecurityGroupRef ID Name
-data Region = Region Name [SecurityGroup]
-data Name = String
-data SecurityGroup = SecurityGroup Name [VM][FirewallRule]
-data VM = VM ID Type Cost CPU RAM AMI State SecurityGroupRef [FirewallRule]
-data ID = String
-data Type = String
-data Cost = Double
-data CPU = Int
-data RAM = Int
-data AMI = String
-data State = Int
-data FirewallRule = FirewallRule Outbound Port IP Protocol
-data Outbound = Boolean
-data Port = String
-data IP = String
-data Protocol = String
+{-# LANGUAGE OverloadedStrings, FlexibleInstances, DeriveGeneric, TemplateHaskell #-}
+
+module SourceModel (Model, Reservation, VM, SecurityGroup, FirewallRule)
+       where
+
+data Model = Model {
+    reservations :: [Reservation]
+  , securityGroups :: [SecurityGroup]
+  } deriving (Show)
+
+data Reservation = Reservation {
+    resID :: String
+  , securityGroupRefs :: [String]
+  , vms :: [VM]
+  } deriving (Show)
+
+data VM = VM {
+    vmID :: String
+  , vmType :: String
+  , cost :: Double
+  , cpu :: Int
+  , ram :: Int
+  , ami :: String
+  , state :: Int
+  , securityGroupRef :: String
+  , load :: Double
+} deriving (Show)
+
+data SecurityGroup = SecurityGroup {
+    sgID :: String
+  , vmRefs :: [String]
+  , firewallRules :: [FirewallRule]
+  } deriving (Show)
+
+data FirewallRule = FirewallRule {
+    fwID :: String
+  , outbound :: Bool
+  , port :: String
+  , ip :: String
+  , protocol :: Int
+  } deriving (Show)
