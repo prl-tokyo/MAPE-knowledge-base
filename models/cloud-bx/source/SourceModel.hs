faf7cb0dce5aef165e765dbe262063c469414683
Lionel Montrieux <lionel@nii.ac.jp>
2016-03-22 17:48:25 +0900

    Order for source records

--- models/cloud-bx/source/SourceModel.hs
+++ models/cloud-bx/source/SourceModel.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE OverloadedStrings, ScopedTypeVariables, FlexibleInstances, DeriveGeneric, TemplateHaskell #-}
+{-# LANGUAGE OverloadedStrings, ScopedTypeVariables, FlexibleInstances, DeriveGeneric, TemplateHaskell, TypeFamilies, ViewPatterns #-}
 
 module SourceModel(
   Model(..)
@@ -11,13 +11,16 @@ module SourceModel(
 data Model = Model {
     reservations :: [Reservation]
   , securityGroups :: [SecurityGroup]
-  } deriving (Show)
+  } deriving (Show, Eq)
 
 data Reservation = Reservation {
     resID :: String
   , securityGroupRefs :: [String]
   , vms :: [VM]
-  } deriving (Show)
+  } deriving (Show, Eq)
+
+instance Ord Reservation where
+  compare res1 res2 = compare (resID res1) (resID res2)
 
 data VM = VM {
     vmID :: String
@@ -29,13 +32,19 @@ data VM = VM {
   , state :: Int
   , securityGroupRef :: String
   , load :: Double
-} deriving (Show)
+} deriving (Show, Eq)
+
+instance Ord VM where
+  compare vm1 vm2 = compare (vmID vm1) (vmID vm2)
 
 data SecurityGroup = SecurityGroup {
     sgID :: String
   , vmRefs :: [String]
   , firewallRules :: [FirewallRule]
-  } deriving (Show)
+  } deriving (Show, Eq)
+
+instance Ord SecurityGroup where
+  compare sg1 sg2 = compare (sgID sg1) (sgID sg2)
 
 data FirewallRule = FirewallRule {
     fwID :: String
@@ -43,4 +52,7 @@ data FirewallRule = FirewallRule {
   , port :: String
   , ip :: String
   , protocol :: String
-  } deriving (Show)
+  } deriving (Show, Eq)
+
+instance Ord FirewallRule where
+  compare fw1 fw2 = compare (fwID fw1) (fwID fw2)
