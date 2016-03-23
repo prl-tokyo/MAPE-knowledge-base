eecc0444df1a7533465bbbaeabf6a0b66ee4f639
Lionel Montrieux <lionel@nii.ac.jp>
2016-03-23 12:26:53 +0900

    Updates Firewall model

---
+++ models/cloud-bx/views/FirewallModel.hs
@@ -0,0 +1,24 @@
+{-# LANGUAGE OverloadedStrings, FlexibleInstances, DeriveGeneric, TemplateHaskell, ScopedTypeVariables #-}
+
+module FirewallModel (
+  FWView(..)
+  , FWRule(..)
+  , FWSG(..)
+  ) where
+
+data FWView = FWView {
+  fwRules :: [FWRule]
+  , fwSecurityGroups :: [FWSG]
+  } deriving (Show, Eq)
+
+data FWRule = FWRule {
+  fwSecurityGroupRefFrom :: String,
+  fwSecurityGroupRefTo :: String,
+  fwPort :: Int,
+  fwProtocol :: String
+  } deriving (Show, Eq)
+
+data FWSG = FWSG {
+  fwsgID :: String,
+  fwsgName :: String
+  } deriving (Show, Eq)
--- models/cloud-bx/views/firewall.hs
+++
@@ -1,5 +0,0 @@
-data FirewallModel = FirewallModel [Node] [(Node, Node, [(Port, Protocol)])]
-data Node = Node Name
-data Name = String
-data Port = Int
-data Protocol = String
