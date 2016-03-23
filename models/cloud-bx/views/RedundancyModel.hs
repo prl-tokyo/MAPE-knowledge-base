de87bf989b3ef749b17623b703dff3b647abdabd
Yijun Yu <yijun.yu@gmail.com>
2016-03-23 09:49:14 +0900

    copy AutoScaling to Redundancy

---
+++ models/cloud-bx/views/RedundancyModel.hs
@@ -0,0 +1,16 @@
+module RedundancyModel(
+  View(..)
+  , VVM(..)
+  ) where
+
+data View = View {
+  vvms :: [VVM]} deriving (Show, Eq)
+
+data VVM = VVM {
+  vvmID :: String
+  , vvmType :: String
+  , vload :: Double
+  } deriving (Show, Eq)
+
+instance Ord VVM where
+  compare vm1 vm2 = compare (vvmID vm1) (vvmID vm2)
--- models/cloud-bx/views/redundancy.hs
+++
@@ -1,5 +0,0 @@
-data Redundancy = Redundancy [Task]
-data Task = Task Name [VM]
-data Name = String
-data VM = VM ID
-data ID = String
