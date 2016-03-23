7a8ec0dc17a0d98ce112f8d3256f6d8d8d9e088d
Lionel Montrieux <lionel@nii.ac.jp>
2016-03-23 12:08:58 +0900

    Refactors `align` in Utils module

---
+++ models/cloud-bx/Utils.hs
@@ -0,0 +1,37 @@
+{-# LANGUAGE TemplateHaskell, ViewPatterns #-}
+module Utils (align) where
+
+import Generics.BiGUL.AST
+import Generics.BiGUL.TH
+import Control.Arrow
+import Data.Maybe
+
+align :: (a -> Bool)
+      -> (a -> b -> Bool)
+      -> BiGUL a b
+      -> (b -> a)
+      -> (a -> Maybe a)
+      -> BiGUL [a] [b]
+align p match b create conceal = Case
+  [ $(normalSV [| null . filter p |] [p| [] |])$
+      $(rearrV [| \[] -> () |])$ Skip
+  , $(adaptiveV [p| [] |])$
+      \ss _ -> catMaybes (map (\s -> if p s then conceal s else Just s) ss)
+  -- view is necessarily nonempty in the cases below
+  , $(normalS [p| (p -> False):_ |])$
+      $(rearrS [| \(s:ss) -> ss |])$
+        align p match b create conceal
+  , $(normal' [| \ss vs -> not (null ss) && p (head ss) && match (head ss) (head vs) |]
+              [| \ss    -> not (null ss) && p (head ss) |])$
+      $(rearrV [| \(v:vs) -> (v, vs) |])$
+        $(rearrS [| \(s:ss) -> (s, ss) |])$
+          b `Prod` align p match b create conceal
+  , $(adaptive [| \ss (v:_) -> isJust (findFirst (\s -> p s && match s v) ss) ||
+                               let s = create v in p s && match s v |])$
+      \ss (v:_) -> maybe (create v:ss) (uncurry (:)) (findFirst (\s -> p s && match s v) ss)
+  ]
+  where
+    findFirst :: (a -> Bool) -> [a] -> Maybe (a, [a])
+    findFirst p [] = Nothing
+    findFirst p (x:xs) | p x       = Just (x, xs)
+    findFirst p (x:xs) | otherwise = fmap (id *** (x:)) (findFirst p xs)
--- models/cloud-bx/bx/RedundancyBX.hs
+++ models/cloud-bx/bx/RedundancyBX.hs
@@ -19,6 +19,7 @@ import GHC.Generics
 import Control.Arrow
 import Data.Maybe
 import AWSModel
+import Utils
 import qualified SourceModel as S
 import qualified RedundancyModel as V --(RView(..), RVM(..))
 
@@ -74,33 +75,3 @@ rvm3 = V.RVM {
   V.rvmID = "vm3",
   V.rSecurityGroupRef = "sg-456"
   }
-
-align :: (a -> Bool)
-      -> (a -> b -> Bool)
-      -> BiGUL a b
-      -> (b -> a)
-      -> (a -> Maybe a)
-      -> BiGUL [a] [b]
-align p match b create conceal = Case
-  [ $(normalSV [| null . filter p |] [p| [] |])$
-      $(rearrV [| \[] -> () |])$ Skip
-  , $(adaptiveV [p| [] |])$
-      \ss _ -> catMaybes (map (\s -> if p s then conceal s else Just s) ss)
-  -- view is necessarily nonempty in the cases below
-  , $(normalS [p| (p -> False):_ |])$
-      $(rearrS [| \(s:ss) -> ss |])$
-        align p match b create conceal
-  , $(normal' [| \ss vs -> not (null ss) && p (head ss) && match (head ss) (head vs) |]
-              [| \ss    -> not (null ss) && p (head ss) |])$
-      $(rearrV [| \(v:vs) -> (v, vs) |])$
-        $(rearrS [| \(s:ss) -> (s, ss) |])$
-          b `Prod` align p match b create conceal
-  , $(adaptive [| \ss (v:_) -> isJust (findFirst (\s -> p s && match s v) ss) ||
-                               let s = create v in p s && match s v |])$
-      \ss (v:_) -> maybe (create v:ss) (uncurry (:)) (findFirst (\s -> p s && match s v) ss)
-  ]
-  where
-    findFirst :: (a -> Bool) -> [a] -> Maybe (a, [a])
-    findFirst p [] = Nothing
-    findFirst p (x:xs) | p x       = Just (x, xs)
-    findFirst p (x:xs) | otherwise = fmap (id *** (x:)) (findFirst p xs)
