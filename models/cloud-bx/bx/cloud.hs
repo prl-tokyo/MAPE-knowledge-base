cc143e897d769f9da11ed07a184beded5ce444c5
Lionel Montrieux <lionel@nii.ac.jp>
2016-03-23 11:41:52 +0900

    Combined example

---
+++ models/cloud-bx/bx/cloud.hs
@@ -0,0 +1,17 @@
+{-# LANGUAGE FlexibleContexts, TemplateHaskell, TypeFamilies, TypeOperators, ViewPatterns #-}
+
+--import Generics.BiGUL
+import Generics.BiGUL.AST
+import Generics.BiGUL.Error
+import Generics.BiGUL.Interpreter
+import Language.Haskell.TH as TH hiding (Name)
+import Generics.BiGUL.TH
+import Control.Monad
+import Data.Char
+import Data.List
+import GHC.Generics
+import Control.Arrow
+import Data.Maybe
+
+import qualified AutoscalingBX as ABX
+import qualified RedundancyBX as RBX hiding(SourceModel(..))
