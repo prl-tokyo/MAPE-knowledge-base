074cf525edf9ca82f17460ffb4a7abe72249d871
Lionel Montrieux <lionel@nii.ac.jp>
2016-03-28 14:59:19 +0900

    Rename VVM to Instance in autoscaling view

--- Haskell/views/AutoScalingModel.hs
+++ Haskell/views/AutoScalingModel.hs
@@ -3,26 +3,26 @@
 
 module AutoScalingModel(
   View(..)
-  , VVM(..)
+  , Instance(..)
   ) where
 
 import Generics.BiGUL.TH
 import GHC.Generics
 
 data View = View {
-  current :: [VVM]
-  , additions :: [VVM]
-  , terminations :: [VVM]
+  current :: [Instance]
+  , additions :: [Instance]
+  , terminations :: [Instance]
   } deriving (Show, Eq)
 
-data VVM = VVM {
-  vvmID :: String
-  , vvmType :: String
-  , vload :: Double
+data Instance = Instance {
+  instID :: String
+  , instType :: String
+  , instload :: Double
   } deriving (Show, Eq)
 
-instance Ord VVM where
-  compare vm1 vm2 = compare (vvmID vm1) (vvmID vm2)
+instance Ord Instance where
+  compare inst1 inst2 = compare (instID inst1) (instID inst2)
 
-deriveBiGULGeneric ''VVM
+deriveBiGULGeneric ''Instance
 deriveBiGULGeneric ''View
