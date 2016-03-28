592886db201e61456214adb989d07679e88e7f52
Lionel Montrieux <lionel@nii.ac.jp>
2016-03-28 15:16:50 +0900

    View and ChangeView in AutoScaling
    Also adds instance types

--- Haskell/views/AutoScalingModel.hs
+++ Haskell/views/AutoScalingModel.hs
@@ -2,17 +2,25 @@
 , TypeFamilies #-}
 
 module AutoScalingModel(
-  View(..)
+  ChangeView(..)
   , Instance(..)
+  , InstanceType(..)
+  , View(..)
   ) where
 
 import Generics.BiGUL.TH
 import GHC.Generics
 
-data View = View {
+data ChangeView = ChangeView {
   current :: [Instance]
   , additions :: [Instance]
   , terminations :: [Instance]
+  , cvInstanceTypes :: [InstanceType]
+  } deriving (Show, Eq)
+
+data View = View {
+  instances :: [Instance]
+  , vInstanceTypes :: [InstanceType]
   } deriving (Show, Eq)
 
 data Instance = Instance {
@@ -24,5 +32,18 @@ data Instance = Instance {
 instance Ord Instance where
   compare inst1 inst2 = compare (instID inst1) (instID inst2)
 
+data InstanceType = InstanceType {
+  typeID :: String
+  , typeCPUs :: Int
+  , typeRAM :: Double
+  , typeCost :: Double
+  } deriving (Show, Eq)
+
+instance Ord InstanceType where
+  compare inst1 inst2 = compare (typeID inst1) (typeID inst2)
+
+
+deriveBiGULGeneric ''ChangeView
 deriveBiGULGeneric ''Instance
+deriveBiGULGeneric ''InstanceType
 deriveBiGULGeneric ''View
