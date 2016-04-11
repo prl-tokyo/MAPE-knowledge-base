7d86837625e4642c03e30c08f4338fafd0c7520c
Lionel Montrieux <lionel@nii.ac.jp>
2016-04-11 17:01:11 +0900

    JSON encode and decode

--- Haskell/views/FirewallModel.hs
+++ Haskell/views/FirewallModel.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE TemplateHaskell
-, TypeFamilies #-}
+, TypeFamilies
+, OverloadedStrings #-}
 
 module FirewallModel (
   Rule(..)
@@ -8,6 +9,7 @@ module FirewallModel (
 
 import Generics.BiGUL.TH
 import GHC.Generics
+import Data.Aeson
 
 data View = View {
   rules :: [Rule]
@@ -20,6 +22,36 @@ data Rule = Rule {
   , port :: String
   , protocol :: String
   } deriving (Show, Eq)
+
+instance FromJSON View where
+    parseJSON (Object v) = View <$>
+                           v .: "rules"
+    -- A non-Object value is of the wrong type, so fail.
+    parseJSON _          = mempty
+
+instance ToJSON View where
+    -- this generates a Value
+    toJSON (View rules) =
+        object ["rules" .= rules]
+
+instance FromJSON Rule where
+    parseJSON (Object v) = Rule <$>
+                           v .: "ruleID" <*>
+                           v .: "securityGroupRefFrom" <*>
+                           v .: "securityGroupRefTo" <*>
+                           v .: "port" <*>
+                           v .: "protocol"
+    -- A non-Object value is of the wrong type, so fail.
+    parseJSON _          = mempty
+
+instance ToJSON Rule where
+    -- this generates a Value
+    toJSON (Rule ruleID securityGroupRefFrom securityGroupRefTo port protocol) =
+        object ["ruleID" .= ruleID
+                , "securityGroupRefFrom" .= securityGroupRefFrom
+                , "securityGroupRefTo" .= securityGroupRefTo
+                , "port" .= port
+                , "protocol" .= protocol]
 
 deriveBiGULGeneric ''View
 deriveBiGULGeneric ''Rule
