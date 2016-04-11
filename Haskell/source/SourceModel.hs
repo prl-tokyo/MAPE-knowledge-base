1facc1a71c0713239eb6254b0b78f3bc61a45e8c
Lionel Montrieux <lionel@nii.ac.jp>
2016-04-11 16:51:42 +0900

    Add JSON encoding and decoding
    These have to be done manually (oh Joy!), because the easy way doesn't play nice with BiGUL's deriveBiGULGenerics

--- Haskell/source/SourceModel.hs
+++ Haskell/source/SourceModel.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE TemplateHaskell
-, TypeFamilies #-}
+, TypeFamilies
+, OverloadedStrings #-}
 
 -- This model defines the structure of the source model. The root is `Model`.
 -- It is assumed that the put behaviour updating this model will not directly
@@ -12,10 +13,16 @@ module SourceModel(
   , SecurityGroup(..)
   , FirewallRule(..)
   , InstanceType(..)
+  , Root(..)
   ) where
 
 import Generics.BiGUL.TH
 import GHC.Generics
+import Data.Aeson
+
+data Root = Root {
+  model :: Model
+} deriving (Show, Eq)
 
 data Model = Model {
   instances :: [Instance]
@@ -64,8 +71,112 @@ data InstanceType = InstanceType {
 instance Ord InstanceType where
   compare inst1 inst2 = compare (typeID inst1) (typeID inst2)
 
+instance FromJSON InstanceType where
+    parseJSON (Object v) = InstanceType <$>
+                           v .: "typeID" <*>
+                           v .: "typeCPUs" <*>
+                           v .: "typeRAM" <*>
+                           v .: "typeCost"
+    -- A non-Object value is of the wrong type, so fail.
+    parseJSON _          = mempty
+
+instance ToJSON InstanceType where
+    -- this generates a Value
+    toJSON (InstanceType typeID typeCPUs typeRAM typeCost) =
+        object ["typeID" .= typeID
+                , "typeCPUs" .= typeCPUs
+                , "typeRAM" .= typeRAM
+                , "typeCost" .= typeCost]
+
+instance FromJSON FirewallRule where
+    parseJSON (Object v) = FirewallRule <$>
+                           v .: "fwRuleID" <*>
+                           v .: "outbound" <*>
+                           v .: "port" <*>
+                           v .: "ip" <*>
+                           v .: "protocol" <*>
+                           v .: "fwStatus"
+    -- A non-Object value is of the wrong type, so fail.
+    parseJSON _          = mempty
+
+instance ToJSON FirewallRule where
+    -- this generates a Value
+    toJSON (FirewallRule fwRuleID outbound port ip protocol fwStatus) =
+        object ["fwRuleID" .= fwRuleID
+                , "outbound" .= outbound
+                , "port" .= port
+                , "ip" .= ip
+                , "protocol" .= protocol
+                , "fwStatus" .= fwStatus]
+
+instance FromJSON SecurityGroup where
+    parseJSON (Object v) = SecurityGroup <$>
+                           v .: "sgID" <*>
+                           v .: "instRefs" <*>
+                           v .: "firewallRules"
+    -- A non-Object value is of the wrong type, so fail.
+    parseJSON _          = mempty
+
+instance ToJSON SecurityGroup where
+    -- this generates a Value
+    toJSON (SecurityGroup sgID instRefs firewallRules) =
+        object ["sgID" .= sgID
+                , "instRefs" .= instRefs
+                , "firewallRules" .= firewallRules]
+
+instance FromJSON Instance where
+    parseJSON (Object v) = Instance <$>
+                           v .: "instID" <*>
+                           v .: "instType" <*>
+                           v .: "ami" <*>
+                           v .: "state" <*>
+                           v .: "instStatus" <*>
+                           v .: "securityGroupRef" <*>
+                           v .: "load"
+    -- A non-Object value is of the wrong type, so fail.
+    parseJSON _          = mempty
+
+instance ToJSON Instance where
+    -- this generates a Value
+    toJSON (Instance instID instType ami state instStatus securityGroupRef load) =
+        object ["instID" .= instID
+                , "instType" .= instType
+                , "ami" .= ami
+                , "state" .= state
+                , "instStatus" .= instStatus
+                , "securityGroupRef" .= securityGroupRef
+                , "load" .= load]
+
+instance FromJSON Model where
+    parseJSON (Object v) = Model <$>
+                           v .: "instances" <*>
+                           v .: "securityGroups" <*>
+                           v .: "instanceTypes"
+    -- A non-Object value is of the wrong type, so fail.
+    parseJSON _          = mempty
+
+instance ToJSON Model where
+    -- this generates a Value
+    toJSON (Model instances securityGroups instanceTypes) =
+        object ["instances" .= instances
+                , "securityGroups" .= securityGroups
+                , "instanceTypes" .= instanceTypes]
+
+instance FromJSON Root where
+    parseJSON (Object v) = Root <$>
+                           v .: "model"
+    -- A non-Object value is of the wrong type, so fail.
+    parseJSON _          = mempty
+
+instance ToJSON Root where
+    -- this generates a Value
+    toJSON (Root model) =
+        object ["model" .= model]
+
 deriveBiGULGeneric ''Model
 deriveBiGULGeneric ''Instance
 deriveBiGULGeneric ''FirewallRule
 deriveBiGULGeneric ''SecurityGroup
 deriveBiGULGeneric ''InstanceType
+deriveBiGULGeneric ''Root
+
