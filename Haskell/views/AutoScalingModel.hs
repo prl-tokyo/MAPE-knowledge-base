f0c83564cf3f4daa4e0bdb1501275e6f74c79f40
Lionel Montrieux <lionel@nii.ac.jp>
2016-04-11 16:57:30 +0900

    JSON encode and decode

--- Haskell/views/AutoScalingModel.hs
+++ Haskell/views/AutoScalingModel.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE TemplateHaskell
-, TypeFamilies #-}
+, TypeFamilies
+, OverloadedStrings #-}
 
 module AutoScalingModel(
   Instance(..)
@@ -9,6 +10,7 @@ module AutoScalingModel(
 
 import Generics.BiGUL.TH
 import GHC.Generics
+import Data.Aeson
 
 data View = View {
   instances :: [Instance]
@@ -34,6 +36,50 @@ data InstanceType = InstanceType {
 instance Ord InstanceType where
   compare inst1 inst2 = compare (typeID inst1) (typeID inst2)
 
+instance FromJSON View where
+    parseJSON (Object v) = View <$>
+                           v .: "instances" <*>
+                           v .: "instanceTypes" 
+    -- A non-Object value is of the wrong type, so fail.
+    parseJSON _          = mempty
+
+instance ToJSON View where
+    -- this generates a Value
+    toJSON (View instances instanceTypes) =
+        object ["instances" .= instances
+                , "instanceTypes" .= instanceTypes]
+
+instance FromJSON Instance where
+    parseJSON (Object v) = Instance <$>
+                           v .: "instID" <*>
+                           v .: "instType" <*>
+                           v .: "instLoad"
+    -- A non-Object value is of the wrong type, so fail.
+    parseJSON _          = mempty
+
+instance ToJSON Instance where
+    -- this generates a Value
+    toJSON (Instance instID instType instLoad) =
+        object ["instID" .= instID
+                , "instType" .= instType
+                , "instLoad" .= instLoad]
+
+instance FromJSON InstanceType where
+    parseJSON (Object v) = InstanceType <$>
+                           v .: "typeID" <*>
+                           v .: "typeCPUs" <*>
+                           v .: "typeRAM" <*>
+                           v .: "typeCost"
+    -- A non-Object value is of the wrong type, so fail.
+    parseJSON _          = mempty
+
+instance ToJSON InstanceType where
+    -- this generates a Value
+    toJSON (InstanceType typeID typeCPUs typeRAM typeCost) =
+        object ["typeID" .= typeID
+                , "typeCPUs" .= typeCPUs
+                , "typeRAM" .= typeRAM
+                , "typeCost" .= typeCost]
 
 deriveBiGULGeneric ''Instance
 deriveBiGULGeneric ''InstanceType
