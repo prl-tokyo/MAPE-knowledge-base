1e8a5310b4130662b945425fff949581715278de
Lionel Montrieux <lionel@nii.ac.jp>
2016-04-11 17:04:05 +0900

    Add JSON encode and decode

--- Haskell/views/RedundancyModel.hs
+++ Haskell/views/RedundancyModel.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE TemplateHaskell
-, TypeFamilies #-}
+, TypeFamilies
+, OverloadedStrings #-}
 
 module RedundancyModel (
   Instance(..)
@@ -8,6 +9,7 @@ module RedundancyModel (
 
 import Generics.BiGUL.TH
 import GHC.Generics
+import Data.Aeson
 
 data View = View {instances :: [Instance]} deriving (Show, Eq)
 
@@ -20,5 +22,32 @@ data Instance = Instance {
 instance Ord Instance where
   compare inst1 inst2 = compare (instID inst1) (instID inst2)
 
+instance FromJSON View where
+    parseJSON (Object v) = View <$>
+                           v .: "instances"
+    -- A non-Object value is of the wrong type, so fail.
+    parseJSON _          = mempty
+
+instance ToJSON View where
+    -- this generates a Value
+    toJSON (View instances) =
+        object ["instances" .= instances]
+
+instance FromJSON Instance where
+    parseJSON (Object v) = Instance <$>
+                           v .: "instID" <*>
+                           v .: "securityGroupRef" <*>
+                           v .: "status"
+    -- A non-Object value is of the wrong type, so fail.
+    parseJSON _          = mempty
+
+instance ToJSON Instance where
+    -- this generates a Value
+    toJSON (Instance instID securityGroupRef status) =
+        object ["instID" .= instID
+                , "securityGroupRef" .= securityGroupRef
+                , "status" .= status]
+
 deriveBiGULGeneric ''Instance
 deriveBiGULGeneric ''View
+
