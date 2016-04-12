b12690d8061d1290d4281211f6330da25cef97a0
Lionel Montrieux <lionel@nii.ac.jp>
2016-04-13 00:17:56 +0900

    Improve transformation

--- Haskell/bx/ExecutionBX.hs
+++ Haskell/bx/ExecutionBX.hs
@@ -1,7 +1,7 @@
 {-# LANGUAGE TemplateHaskell
 , TypeFamilies #-}
 
-module ExecutionBX (executionUpd) where
+module ExecutionBX where
 
 import Generics.BiGUL.AST
 import Generics.BiGUL.Error
@@ -11,57 +11,65 @@ import Generics.BiGUL.TH
 import Control.Monad
 import Data.Char
 import Data.List
-import GHC.Genericsx
+import GHC.Generics
 import Control.Arrow
 import Data.Maybe
 import Utils
 import qualified SourceModel as S
 import qualified ExecutionModel as V
 
-executionUpd :: BiGUL S.Model V.View
+--executionUpd :: BiGUL S.Model V.View
+--executionUpd = addUpd `Prod` termUpd
+
+addUpd :: BiGUL S.Model V.View
+addUpd = $(update [p| V.View {
+                       V.terminations = terminations,
+                       V.additions = additions
+               }|] [p| S.Model {
+                       S.instances = additions
+               }|] [d| additions = instListAddAlign;
+               		   terminations = Skip
+                |])
+
+termUpd :: BiGUL S.Model V.View
+termUpd = $(update [p| V.View {
+                       V.terminations = terminations,
+                       V.additions = additions
+               }|] [p| S.Model {
+                       S.instances = terminations
+               }|] [d| terminations = instListTermAlign;
+                       additions = Skip
+                |])
+
+data Instance = Instance {
+    instID :: String
+    , instType :: String
+    , ami :: String
+    , state :: Int
+    , securityGroupRef :: String}
 
 instUpd :: BiGUL S.Instance V.Instance
 instUpd = $(update [p| V.Instance {
-        V.instID = instID
-        , V.instType = instType
-        , V.ami = ami
-        , V.state = state
-        , V.securityGroupRef = securityGroupRef
-    }|] [p| S.Instance {
-        S.instID = instID
-        , S.instType = instType
-        , S.ami = ami
-        , S.state = state
-        , S.securityGroupRef = securityGroupRef
-    }|] [d| instID = Replace;
-            instType = Replace;
-            ami = Replace;
-            state = Replace;
-            securityGroupRef = Replace
-    |])
+                       V.instID = instID,
+                       V.instType = instType,
+                       V.ami = ami,
+                       V.state = state,
+                       V.securityGroupRef = securityGroupRef
+               }|] [p| S.Instance {
+                       S.instID = instID,
+                       S.instType = instType,
+                       S.ami = ami,
+                       S.state = state,
+                       S.securityGroupRef = securityGroupRef
+               }|] [d| instID = Replace;
+                       instType = Replace;
+                       ami = Replace;
+                       state = Replace;
+                       securityGroupRef = Replace
+  |])
 
-fwUpd :: BiGUL S.FirewallRule V.FirewallRule
-fwUpd = $(update [p| V.FirewallRule {
-		V.fwRuleID = fwRuleID
-		, V.outbound = outbound
-		, V.port = port
-		, V.ip = ip
-		, V.protocol = protocol
-	} |] [p| S.FirewallRule {
-		S.fwRuleID = fwRuleID
-		, S.outbound = outbound
-		, S.port = port
-		, S.ip = ip
-		, S.protocol = protocol
-	} |] [d| fwRuleID = Replace;
-		     outbound = Replace;
-		     port = Replace;
-		     ip = Replace;
-		     protocol = Replace;
-	}|])
-
-instListAlign :: BiGUL [S.Instance] [V.Instance]
-instListAlign = align (const True)
+instListAddAlign :: BiGUL [S.Instance] [V.Instance]
+instListAddAlign = align (\s -> S.instStatus s == 1)
   (\ s v -> S.instID s == V.instID v)
   ($(update [p| v |] [p| v |] [d| v = instUpd |]))
   (\v -> S.Instance {
@@ -73,6 +81,19 @@ instListAlign = align (const True)
       , S.load = 0.00
       , S.securityGroupRef = V.securityGroupRef v
       })
-  (\s -> const Nothing)
+  (\s -> Nothing)
 
-fwListAlign :: BiGUL [S.FirewallRule] [V.FirewallRule]
+instListTermAlign :: BiGUL [S.Instance] [V.Instance]
+instListTermAlign = align (\s -> S.instStatus s == 2)
+  (\ s v -> S.instID s == V.instID v)
+  ($(update [p| v |] [p| v |] [d| v = instUpd |]))
+  (\v -> S.Instance {
+      S.instID = V.instID v
+      , S.instType = V.instType v
+      , S.instStatus = 0
+      , S.ami = V.ami v
+      , S.state = V.state v
+      , S.load = 0.00
+      , S.securityGroupRef = V.securityGroupRef v
+      })
+  (\s -> Nothing)
