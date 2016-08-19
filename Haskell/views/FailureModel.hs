5789e214ca4c474b1e1881ca7e66ec245d8af146
Lionel Montrieux <lionel@nii.ac.jp>
2016-08-19 19:28:18 +0900

    Failure model

---
+++ Haskell/views/FailureModel.hs
@@ -0,0 +1,46 @@
+{-# LANGUAGE TemplateHaskell
+, TypeFamilies
+, OverloadedStrings #-}
+
+module FailureModel(
+
+) where
+
+import Generics.BiGUL.TH
+import GHC.Generics
+import Data.Aeson
+
+data FailureView = FailureView {
+  instances :: [Instance]
+} deriving (Show, Eq)
+
+data Instance = Instance {
+  instID :: String
+  , instType :: String
+  , instResponseTime :: Int
+} deriving (Show, Eq)
+
+instance Ord Instance where
+  compare inst1 inst2 = compare (instID inst1) (instID inst2)
+
+instance FromJSON FailureView where
+  parseJSON (Object v) = FailureView <$>
+                         v .: "instances"
+  parseJSON _          = mempty
+
+instance ToJSON FailureView where
+  toJSON(FailureView instances) =
+    object["instances" .= instance]
+
+instance FromJSON Instance where
+  parseJSON (Object v) = Instance <$>
+                         v .: "instID" <*>
+                         v .: "instType" <*>
+                         v .: "instResponseTime"
+  parseJSON _          = mempty
+
+instance ToJSON Instance where
+  toJSON (Instance instID intType instResponseTime) =
+    object ["instID" .= instID
+            , "instType" .= instType
+            , "instResponseTime" .= instResponseTime]
